4821ef1dfbcd6b7f7376443da43f1baae3417b3f
Eric Penrod <emp923@gmail.com>
2022-05-30 10:31:11 -0400

    adds greeting request mapping

--- src/main/java/com/epenrod/controller/HelloController.java
+++ src/main/java/com/epenrod/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.epenrod.controller;
 
+import com.epenrod.model.Greeter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
@@ -9,6 +12,17 @@ public class HelloController {
     @RequestMapping("/")
     public String displayHomePage() {
         return "homePage";
+    }
+
+    @RequestMapping("/greeting")
+    public String displayGreeting(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+        Greeter greeter = new Greeter(name);
+        request.setAttribute("greeting", greeter.getGreeting());
+
+        return "greeting";
+
     }
 
 }
