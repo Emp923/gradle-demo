b10a59c2a4abd1005a67fec2b884002c30aa8806
Eric Penrod <emp923@gmail.com>
2022-05-30 10:28:54 -0400

    adds greeter model

---
+++ src/main/java/com/epenrod/model/Greeter.java
@@ -0,0 +1,15 @@
+package com.epenrod.model;
+
+public class Greeter {
+
+    private String name;
+
+    public Greeter(String name) {
+        this.name = name;
+    }
+
+    public String getGreeting() {
+        return "Hello, " + name + "!";
+    }
+
+}
